8e5cfd8dd2784b1878166def0f7624e34ae4560c
haxom <haxom@haxom.net>
2018-10-09 23:26:18 +0200

    managing GPIO / removing threading / better flipping process

--- Template/process/template_process.py
+++ Template/process/template_process.py
@@ -8,13 +8,15 @@ __version__     = '0.1a'
 # pymodbus
 from pymodbus.client.sync import ModbusTcpClient
 
-# Threading
-from threading import Thread
-from time import sleep
-
 # System
 import sys
 import signal
+from time import sleep
+
+# Params
+modbus_server_ip = '127.0.0.1'
+modbus_server_port = 5002
+
 
 def signal_handler(sig, frame):
     print 'CTRL+C pressed, exiting...'
@@ -22,30 +24,39 @@ def signal_handler(sig, frame):
 signal.signal(signal.SIGINT, signal_handler)
 
 def initdb():
-    client = ModbusTcpClient('127.0.0.1', 5002)
-    #client = ModbusTcpClient('127.0.0.1', 502)
+    client = ModbusTcpClient(modbus_server_ip, modbus_server_port)
     for registre in range(1,21):
         client.write_coil(registre, False)
 
-def updatedb():
+def loop_process():
     # Main Process (template = flip-flop)
-    while True:
-        sleep(0.5)
-        try:
-            client = ModbusTcpClient('127.0.0.1', 5002)
-            #client = ModbusTcpClient('127.0.0.1', 502)
+    err_count = 0
+    coils_count = 20
 
-            coils = client.read_coils(1,21)
-            for i in range(20):
-                if coils[i]:
-                    client.write_coil(i+1, False)
-                else:
-                    client.write_coil(i+1, True)
+    while True:
+        sleep(1)
+        try:
+            client = ModbusTcpClient(modbus_server_ip, modbus_server_port)
+
+            coils = client.read_coils(0, count=coils_count)
+            coils = coils.bits[:coils_count]
+            # flipping booleans from list coils
+            coils = [not i for i in coils]
+            client.write_coils(0, coils)
+            updateGPIO(coils)
         except Exception as err:
             print '[error] %s' % err
+            err_count += 1
+            if err_count == 5:
+                print '[error] 5 errors happened in the process ! exiting...'
+                sys.exit(1)
 
+def updateGPIO(coils=[]):
+    for i in range(len(coils)):
+        # do something with GPIO
+        print 'coils[%d] = %d' % (i, coils[i])
+    print '**********************************************'
 
 if __name__ == '__main__':
-    th = Thread(target=updatedb)
-    th.start()
-    th.join()
+    initdb()
+    loop_process()
