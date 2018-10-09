3841216341e2880657098309931814be6046ec47
haxom <haxom@haxom.net>
2018-10-09 23:12:10 +0200

    transfering GPIO management to template_process / threading not necessary anymore

--- Template/process/template_server.py
+++ Template/process/template_server.py
@@ -13,33 +13,19 @@ from pymodbus.datastore import ModbusSlaveContext, ModbusServerContext
 
 from pymodbus.client.sync import ModbusTcpClient
 
-# Rasp GPIO
-
-# Threading
-from threading import Thread
-from time import sleep
-
 # System
 import sys
 import signal
+
+# Params
+listen_int = '127.0.0.1'
+listen_port = 5002
 
 def signal_handler(sig, frame):
     print 'CTRL+C pressed, exiting...'
     sys.exit(0)
 signal.signal(signal.SIGINT, signal_handler)
 
-
-def updateGPIO():
-    while True:
-        sleep(0.5)
-        try:
-            client = ModbusTcpClient('127.0.0.1', 5002)
-            #client = ModbusTcpClient('127.0.0.1', 502)
-            coils = client.read_coils(1,20)
-            for i in range(20):
-                print 'Coil %d = %d' % (i, coils.bits[i])
-        except Exception as err:
-            print '[error] %s' % err
 
 def init():
     store = ModbusSlaveContext(
@@ -58,10 +44,13 @@ def init():
     identity.ModelName = ''
     identity.MajorMinorRevision = ''
 
-    StartTcpServer(context, identity=identity, address=('localhost', 5002))
-    #StartTcpServer(context, identity=identity, address=('localhost', 502)
+    print 'Launching Modbus server, listening on %s:%d' % (listen_int, listen_port)
+    StartTcpServer(context, identity=identity, address=(listen_int, listen_port))
 
 if __name__ == '__main__':
-    th = Thread(target=updateGPIO)
-    th.start()
-    init()
+    try:
+        init()
+    except Exception as err:
+        print '[error] Can\'t init Modbus server ...'
+        print '[error] %s' % err
+        sys.exit(1)
