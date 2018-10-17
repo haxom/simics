e6165737ab18fe8ace4e1489cd69869b082a771f
haxom <haxom@haxom.net>
2018-10-18 01:01:22 +0200

    web IHM done

--- Eolienne/web/index.php
+++ Eolienne/web/index.php
@@ -25,6 +25,13 @@
 					$output['registers'][$key] = PhpType::bytes2unsignedInt($value);
 				print json_encode($output);
 				exit();
+
+			case "manualStop":
+				$modbus->writeMultipleCoils($unitId, 0, [0]);
+				exit();
+			case "manualStart":
+				$modbus->writeMultipleCoils($unitId, 0, [1]);
+				exit();
 		}
 	}
 ?>
@@ -37,19 +44,62 @@
 
 		function updateCoils()
 		{
-			html = "";
-			for(var i in coils)
-				html += "["+i+"] => "+coils[i]+"<br />";
-			document.getElementById('div_coils').innerHTML = html;
+			var div_manual = document.getElementById('div_status_manual');
+			var button_stop = document.getElementById('button_stop');
+			var div_auto = document.getElementById('div_status_auto');
+			var button_start = document.getElementById('button_start');
+			if(coils[0] == false)
+			{
+				div_manual.style.backgroundColor = "red";
+				button_stop.disabled = true;
+				button_start.disabled = false;
+			}
+			if(coils[0] == true)
+			{
+				div_manual.style.backgroundColor = "green";
+				button_stop.disabled = false;
+				button_start.disabled = true;
+			}
+			if(coils[1] == false)
+				div_auto.style.backgroundColor = "red";
+			if(coils[1] == true)
+				div_auto.style.backgroundColor = "green";
 		}
 
 		function updateRegisters()
 		{
-			html = "";
-			for(var i in registers)
-				html += "["+i+"] => "+registers[i]+"<br />";
-			document.getElementById('div_registers').innerHTML = html;
+			document.getElementById('input_wind_speed').value = registers[0] + " m/s";
+			document.getElementById('input_power_production').value = registers[1] + " kW";
+		}
 
+		function manualStop()
+		{
+			var xhr = new XMLHttpRequest();
+			xhr.onreadystatechange = function()
+			{
+				if(this.readyState == XMLHttpRequest.DONE)
+				{
+					updateCoils();
+					updateRegisters();
+				}
+			}
+			xhr.open('GET', 'index.php?act=manualStop', true);
+			xhr.send(null);
+		}
+
+		function manualStart()
+		{
+			var xhr = new XMLHttpRequest();
+			xhr.onreadystatechange = function()
+			{
+				if(this.readyState == XMLHttpRequest.DONE)
+				{
+					updateCoils();
+					updateRegisters();
+				}
+			}
+			xhr.open('GET', 'index.php?act=manualStart', true);
+			xhr.send(null);
 		}
 
 		function updateData()
@@ -75,11 +125,31 @@
 </head>
 <body>
 <center><h1>EolienneMonitor</h1></center>
-<h3>Coils</h3>
-<div id="div_coils">
-</div>
-<h3>Registers</h3>
-<div id="div_registers">
-</div>
+<center>
+<table style="border-collapse: collapse;" width="80%">
+<tr>
+<td width="10%" style="border: 1px solid black;" valign="top">
+<center><b>[ STATUS ]</b></center><br />
+<div id="div_status_manual" style="border-radius: 5px; border: 1px solid black; display: inline;background-color: red;" width="25px">&nbsp; &nbsp; &nbsp; &nbsp;</div> Manual<br /><br />
+<div id="div_status_auto" style="border-radius: 5px; border: 1px solid black; display: inline; background-color: red;" width="25px">&nbsp; &nbsp; &nbsp; &nbsp;</div> Automatic<br /> <br />
+
+<center><b>[ ACTIONS ]</b></center><br />
+<center>
+<input style="text-align: center" onclick="manualStart()" type="button" id="button_start" value="START" disabled><br />
+<input style="text-align: center" onclick="manualStop()" type="button" id="button_stop" value="STOP" disabled>
+</center>
+</td>
+<td width="5%" style="text-align: center; border: 1px solid black" valign="top">
+Wind speed <input style="text-align: center;" id="input_wind_speed" value="0 m/s" readonly>
+</td>
+<td width="80%" style="border: 1px solid black;">
+<center><img src="eolienne.png"></center>
+</td>
+<td width="5%" style="border: 1px solid black; text-align: center" valign="bottom">
+Power production (instant) <input style="text-align: center;" id="input_power_production" value="0 kW" readonly>
+</td>
+</tr>
+</table>
+</center>
 </body>
 </html>
