a5cf3df059be8349f384217d09361b186ecad3c1
haxom <haxom@haxom.net>
2019-05-18 17:20:20 +0200

    Add breakable eolienne

--- Eolienne/process/eolienne_process.py
+++ Eolienne/process/eolienne_process.py
@@ -50,13 +50,19 @@ def initdb():
         # 0 : eolienne status (manual)
         # 1 : eolienne status (wind speed control, should be between 15km/h and 90km/h)
         #                     (corresponding to 4m/s and 25m/s)
+        # 25 : eolienne broken status
         #
         # Holding registers table
-        # 10 : wind speed (m/s)
-        # 11 : power production (kW)
+        # 0 : wind speed (m/s)
+        # 1 : power production (kW)
+        # 2-9 : not used
+        # 10 : wind min speed (4m/s)
+        # 11 : wind max (25 m/s)
         
         client.write_coils(0, [True, True], unit=UNIT)
+        client.write_coils(25, [False], unit=UNIT)
         client.write_registers(0, [speed, 0], unit=UNIT)
+        client.write_registers(10, [4, 25], unit=UNIT)
     except Exception as err:
         print '[error] Can\'t init the Modbus coils'
         print '[error] %s' % err
@@ -104,6 +110,9 @@ def loop_process():
             registers = client.read_holding_registers(0, count=2, unit=UNIT).registers
             registers = registers[:2]
 
+            speed_min, speed_max = client.read_holding_registers(10, 2, unit=UNIT).registers
+            broken = client.read_coils(25, count=1, unit=UNIT).bits[0]
+
             # update wind speed
             ## speed = registers[0]
             global speed
@@ -123,6 +132,13 @@ def loop_process():
                 speed = 30
             registers[0] = speed
 
+            # broken
+            if broken:
+                print '[broken eolienne]'
+                registers[1] = 0
+                client.write_registers(0, registers, unit=UNIT)
+                updateGPIO(coils, gust_state)
+                continue
             # manual stop
             if not coils[0]:
                 print '[stop manually]'
@@ -131,7 +147,7 @@ def loop_process():
                 updateGPIO(coils, gust_state)
                 continue
             # wind speed to slow/quick
-            if registers[0] < 4 or registers[0] > 25:
+            if speed < speed_min or speed > speed_max:
                 print '[stop due to the wind speed] (%d m/s)' % registers[0]
                 coils[1] = False
                 client.write_coil(1, False, unit=UNIT)
@@ -139,16 +155,35 @@ def loop_process():
                 client.write_registers(0, registers, unit=UNIT)
                 updateGPIO(coils, gust_state)
                 continue
-            else:
-                coils[1] = True
+
+            powerloss = 0 # 0 % of lost
+            # unwanted case : Wind speed < 4 m/s will consume power production (thermic loss)
+            if speed < 4:
+                powerloss = 25 
+            if speed < 2:
+                powerloss = 50
+
+            # unwanted case : Wind speed > 25 m/s will break the eolienne
+            if speed > 25:
+                broken = True
+                print '[wind breaks the eolienne]'
+                client.write_coil(25, True, unit=UNIT)
+                registers[1] = 0
+                client.write_regiser(0, registers, unit=UNIT)
+                continue
 
             # otherwise, running
             # [production] P = 0.29 * D^2 * v^3
             #   P = power (W)
             #   D = rotor diameter = 24m
             #   v = wind speed (m/s)
+            coils[1] = True
 
-            registers[1] = (0.29 * pow(24, 2) * pow(registers[0], 3))/1000
+            power = (0.29 * pow(24, 2) * pow(registers[0], 3))/1000
+            if powerloss != 0:
+                power = int(power*(1-powerloss/100))
+            registers[1] = power
+
             print 'producting %d kW (%d m/s)' % (registers[1], registers[0])
 
             client.write_coils(0, coils, unit=UNIT)
