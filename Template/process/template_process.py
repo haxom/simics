65905493f4b504526ff8f0a055216e1fdf552345
haxom <haxom@haxom.net>
2021-11-14 11:24:38 +0100

    code-style enhanced

--- Template/process/template_process.py
+++ Template/process/template_process.py
@@ -1,41 +1,38 @@
 #!/usr/bin/env python
-#coding=utf8
-__author__      = 'haxom'
-__email__       = 'haxom@haxom.net'
-__file__        = 'template_process.py'
-__version__     = '1.0'
+# coding=utf8
+__author__ = 'haxom'
+__email__ = 'haxom@haxom.net'
+__file__ = 'template_process.py'
+__version__ = '1.0'
 
-# Options
-modbus_server_ip = '127.0.0.1'
-modbus_server_port = 502
-GPIO = False
-UNIT = 0x42
+import signal
+# System
+import sys
+# Only use to init Modbus coils with random values
+from random import getrandbits as randbits
+from time import sleep
 
 # pymodbus
 from pymodbus.client.sync import ModbusTcpClient
 
-# System
-import sys
-import signal
-from time import sleep
-
-# GPIO
-if GPIO:
-    import RPi.GPIO as GPIO
-
-# Only use to init Modbus coils with random values
-from random import getrandbits as randbits
+# Options
+modbus_server_ip = '127.0.0.1'
+modbus_server_port = 502
+UNIT = 0x42
 
 
 def signal_handler(sig, frame):
     print('CTRL+C pressed, exiting...')
     sys.exit(0)
+
+
 signal.signal(signal.SIGINT, signal_handler)
+
 
 def initdb():
     try:
         client = ModbusTcpClient(modbus_server_ip, modbus_server_port)
-        for registre in range(0,10):
+        for registre in range(0, 10):
             client.write_coil(registre, bool(randbits(1)), unit=UNIT)
             client.write_register(registre, bool(randbits(1)), unit=UNIT)
     except Exception as err:
@@ -44,18 +41,11 @@ def initdb():
         print('[error] exiting...')
         sys.exit(1)
 
-def initGPIO():
-    if GPIO:
-        GPIO.setmode(GPIO.BCM)
-        # GPIO 2 to 21 are usable as I/O
-        for i in range(2, 22):
-            GPIO.setup(i, GPIO.OUT)
 
 def loop_process():
     # Main Process (template = flip-flop)
     err_count = 0
     registre_count = 10
-
 
     while True:
         sleep(1)
@@ -68,7 +58,11 @@ def loop_process():
             coils = [not i for i in coils]
             client.write_coils(0, coils)
 
-            registers = client.read_holding_registers(0, count=registre_count, unit=UNIT)
+            registers = client.read_holding_registers(
+                0,
+                count=registre_count,
+                unit=UNIT
+            )
             registers = registers.registers[:registre_count]
             registers = [i+1 for i in registers]
             client.write_registers(0, registers, unit=UNIT)
@@ -81,23 +75,19 @@ def loop_process():
                 print('[error] 5 errors happened in the process ! exiting...')
                 sys.exit(1)
 
+
 def updateGPIO(coils=[], registers=[]):
-    if GPIO:
-        # coils 0 -> 19 aligned with GPIO pins 2 -> 21
-        for i in range(len(coils)):
-            GPIO.output(i+2, coils[i])
-    else:
-        for i in range(len(coils)):
-            # do something with GPIO
-            print('coils[%d] = %d' % (i, coils[i]))
-        print('**********************************************')
-        for i in range(len(registers)):
-            # do something with GPIO
-            print('registers[%d] = %d' % (i, registers[i]))
-        print('**********************************************')
-    
+    for i in range(len(coils)):
+        # do something with GPIO
+        print('coils[%d] = %d' % (i, coils[i]))
+    print('**********************************************')
+    for i in range(len(registers)):
+        # do something with GPIO
+        print('registers[%d] = %d' % (i, registers[i]))
+    print('**********************************************')
+
+
 if __name__ == '__main__':
     sleep(10)
     initdb()
-    initGPIO()
     loop_process()
