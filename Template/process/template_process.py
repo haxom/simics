9696060a2d87bd232b5091c3ce26e09745de8c5a
haxom <haxom@haxom.net>
2026-05-22 16:40:40 +0200

    migrate pymodbus to 3.12.1

--- Template/process/template_process.py
+++ Template/process/template_process.py
@@ -3,7 +3,7 @@
 __author__ = "haxom"
 __email__ = "haxom@haxom.net"
 __file__ = "template_process.py"
-__version__ = "1.3"
+__version__ = "1.5"
 
 import signal
 # System
@@ -39,12 +39,12 @@ def initdb():
             client.write_coil(
                 address=registre,
                 value=bool(randbits(1)),
-                slave=UNIT
+                device_id=UNIT
             )
             client.write_register(
                 address=registre,
                 value=bool(randbits(1)),
-                slave=UNIT
+                device_id=UNIT
             )
     except Exception as err:
         print("[error] Can't init the Modbus coils")
@@ -69,21 +69,21 @@ def loop_process():
             coils = client.read_coils(
                 address=0,
                 count=registre_count,
-                slave=UNIT
+                device_id=UNIT
             )
             coils = coils.bits[:registre_count]
             # flipping booleans from list coils
             coils = [not i for i in coils]
-            client.write_coils(address=0, values=coils, slave=UNIT)
+            client.write_coils(address=0, values=coils, device_id=UNIT)
 
             registers = client.read_holding_registers(
                 address=0,
                 count=registre_count,
-                slave=UNIT
+                device_id=UNIT
             )
             registers = registers.registers[:registre_count]
             registers = [i+1 for i in registers]
-            client.write_registers(address=0, values=registers, slave=UNIT)
+            client.write_registers(address=0, values=registers, device_id=UNIT)
 
             updateGPIO(coils, registers)
         except Exception as err:
--- Template/process/template_server.py
+++ Template/process/template_server.py
@@ -3,15 +3,15 @@
 __author__ = "haxom"
 __email__ = "haxom@haxom.net"
 __file__ = "template_server.py"
-__version__ = "1.3"
+__version__ = "1.5"
 
 import signal
 # System
 import sys
 
 from pymodbus.datastore import (ModbusSequentialDataBlock, ModbusServerContext,
-                                ModbusSlaveContext)
-from pymodbus.device import ModbusDeviceIdentification
+                                ModbusDeviceContext)
+from pymodbus.pdu.device import ModbusDeviceIdentification
 # pymodbus
 from pymodbus.server import StartTcpServer
 
@@ -19,6 +19,12 @@ from pymodbus.server import StartTcpServer
 listen_int = "0.0.0.0"
 listen_port = 502
 UNIT = 0x42
+
+# PyModbus function codes
+FC_DI = 2
+FC_CO = 1
+FC_IR = 4
+FC_HO = 3
 
 
 def signal_handler(sig, frame):
@@ -30,15 +36,15 @@ signal.signal(signal.SIGINT, signal_handler)
 
 
 def init():
-    slaves = {
-        UNIT: ModbusSlaveContext(
+    devices = {
+        UNIT: ModbusDeviceContext(
             di=ModbusSequentialDataBlock(0, [17]*100),
             co=ModbusSequentialDataBlock(0, [17]*100),
             hr=ModbusSequentialDataBlock(0, [17]*100),
             ir=ModbusSequentialDataBlock(0, [17]*100)
         )
     }
-    context = ModbusServerContext(slaves=slaves, single=False)
+    context = ModbusServerContext(devices=devices, single=False)
 
     identity = ModbusDeviceIdentification()
     identity.VendorName = "HAXOM"
@@ -46,7 +52,7 @@ def init():
     identity.VendorUrl = "https://github.com/haxom/"
     identity.ProductName = "SIMU-ICS"
     identity.ModelName = "TEMPLATE"
-    identity.MajorMinorRevision = "1.3.0"
+    identity.MajorMinorRevision = "1.5.0"
 
     print(f"Modbus slave launched on {listen_int}:{listen_port}")
     StartTcpServer(
